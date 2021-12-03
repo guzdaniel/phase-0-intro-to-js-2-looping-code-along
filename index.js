f4df5fc7cab03cf023364b281a190a3afb668b52
Daniel Guzman <guzdan@live.com>
2021-12-02 20:52:05 -0600

    commit tree fe4aaadaad5d7df6c7b3d80ef0abd5c3b884a5b3

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+// for (let age = 30; age < 40; age++){
+//     console.log(`I'm ${age} years old. Happy birthday
+//     to me!`);
+//    //debugger;
+// }
+
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts){
+//     for (let i = 0; i < gifts.length; i++){
+//         console.log(`wrapped ${gifts[i]} and added a bow!`);
+//         debugger;
+//     }
+
+//     return gifts;
+// }
+
+// wrapGifts(gifts);
+
+function writeCards(names,event){
+    const messages = [];
+
+    for(let i = 0; i < names.length; i++)
+    {
+        
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages;
+    
+}
+const array = writeCards(["bob", "fabiola", "martin"],"birthday")
+
+
+
+function countDown(int){
+    while(int > 0){
+        console.log(int--)
+        if (int === 0){
+            console.log(int)
+        }
+    }
+}
